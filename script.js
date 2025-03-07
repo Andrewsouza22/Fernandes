3afc6067c03f272f4e407b69e0264e55e615860e
Andrewsouza22 <andrew.souza1520@gmail.com>
2025-03-07 17:01:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-const { produtos } = require("./produtos");
+// Lista de produtos
+const produtos = [
+    {
+        id: 1,
+        nome: "Gola Polo - Tommy Hilfilger",
+        preco: 120.00,
+        imagem: "images/camisa-azul.jpg"
+    },
+    {
+        id: 2,
+        nome: "Moda Casual",
+        preco: 139.99,
+        imagem: "images/calca-jeans.jpg"
+        
+    },
+    {
+        id: 3,
+        nome: "Nike Dunk The North Face",
+        preco: 300.00,
+        imagem: "images/sapato-preto.jpg"
+    }
+];
 
 // Função para gerar a lista de produtos
 function gerarListaProdutos() {
@@ -16,5 +37,30 @@ function gerarListaProdutos() {
     });
 }
 
-// Chamar a função para gerar a lista de produtos
-gerarListaProdutos();
+// Função para adicionar produto ao carrinho
+function adicionarProdutoAoCarrinho(produto) {
+    const carrinho = document.querySelector('.carrinho');
+    const quantidade = parseInt(carrinho.textContent.match(/\d+/)[0]);
+    carrinho.textContent = `Carrinho (${quantidade + 1})`;
+}
+
+// Função para remover produto do carrinho
+function removerProdutoDoCarrinho(produto) {
+    const carrinho = document.querySelector('.carrinho');
+    const quantidade = parseInt(carrinho.textContent.match(/\d+/)[0]);
+    carrinho.textContent = `Carrinho (${quantidade - 1})`;
+}
+
+// Função para comprar produto
+function comprarProduto(produto) {
+    adicionarProdutoAoCarrinho(produto);
+    alert(`Você comprou o produto ${produto}!`);
+}
+
+// Adicionar evento de clique ao botão de comprar
+document.querySelectorAll('.comprar').forEach(botao => {
+    botao.addEventListener('click', () => {
+        const produto = botao.parentNode.querySelector('h3').textContent;
+        comprarProduto(produto);
+    });
+});
